623114a059c546f0b4ae179fa68ed470d3ebbe20
mehmetgulek <mehmetgulek@yahoo.com>
2020-07-10 12:49:54 +0300

    Diagram.Net project added, new Windows Forms project and an empty designer added

---
+++ EK.TPM.StateMachine/EK.TPM.Diagram/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace EK.TPM.Diagram
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.canvas = new Dalssoft.DiagramNet.Designer();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.AutoScroll = true;
+            this.canvas.BackColor = System.Drawing.SystemColors.Window;
+            this.canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.canvas.Location = new System.Drawing.Point(0, 0);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(987, 481);
+            this.canvas.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(987, 481);
+            this.Controls.Add(this.canvas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Dalssoft.DiagramNet.Designer canvas;
+    }
+}
+
